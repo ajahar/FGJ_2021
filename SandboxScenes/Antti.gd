4142dc63e8734575050c0c2acf7604d4259f1679
Antti Harju <antti.harju@saunalahti.fi>
2021-01-30 00:43:26 +0200

    Act in sandbox on keypress

--- SandboxScenes/Antti.gd
+++ SandboxScenes/Antti.gd
@@ -2,8 +2,10 @@ extends Node
 
 onready var player: KinematicBody = $Player
 onready var camera: Camera = $Camera
+onready var ball: RigidBody = $Ball
 
 var camera_distance: Vector3
+var move: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,6 +14,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	camera.transform.origin = player.transform.origin - camera_distance
+	if Input.is_action_just_pressed("ui_accept"):
+		ball.sleeping = false
+		move = true
 
 func _physics_process(delta):
-	player.move_and_collide(Vector3.FORWARD * delta * 5)
+	if move:
+		player.move_and_collide(Vector3.FORWARD * delta * 5)
