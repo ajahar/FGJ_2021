8544a7665e8e40bd9a305c8e2520d7fcf9911e6c
Antti Harju <antti.harju@saunalahti.fi>
2021-01-30 19:08:56 +0200

    Follow camera

--- SandboxScenes/Antti.gd
+++ SandboxScenes/Antti.gd
@@ -5,19 +5,26 @@ onready var camera: Camera = $Camera
 onready var ball: RigidBody = $Ball
 
 var camera_distance: Vector3
-var move: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	camera_distance = player.transform.origin - camera.transform.origin
+	camera_distance = player.transform.origin + camera.transform.origin
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	camera.transform.origin = player.transform.origin - camera_distance
-	if Input.is_action_just_pressed("ui_accept"):
-		ball.sleeping = false
-		move = true
-
+	camera.transform = player.transform
+	camera.translate(camera_distance)
+	camera.look_at(player.transform.origin, Vector3.UP)
+	
 func _physics_process(delta):
-	if move:
-		player.move_and_collide(Vector3.FORWARD * delta * 5)
+	var movement = Vector3.ZERO
+	if Input.is_action_pressed("forward"):
+		movement += -player.transform.basis.z
+	if Input.is_action_pressed("right"):
+		player.rotate(Vector3.UP, deg2rad(-5))
+	if Input.is_action_pressed("left"):
+		player.rotate(Vector3.UP, deg2rad(5))
+
+	if movement.length() > 0:
+		player.move_and_slide(movement * 5, Vector3.UP)
+		
