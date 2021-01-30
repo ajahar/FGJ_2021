76b9c102803b1d90d91cdb945004861b8dda8095
Antti Harju <antti.harju@saunalahti.fi>
2021-01-30 22:08:52 +0200

    Separate player actor scene

---
+++ Actors/Player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody
+
+const MOVEMENT_SPEED = 5
+const ROTATION_SPEED = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _physics_process(delta):
+	var movement = Vector3.ZERO
+	if Input.is_action_pressed("forward"):
+		movement += -self.transform.basis.z
+	if Input.is_action_pressed("right"):
+		self.rotate(Vector3.UP, deg2rad(-ROTATION_SPEED))
+	if Input.is_action_pressed("left"):
+		self.rotate(Vector3.UP, deg2rad(ROTATION_SPEED))
+
+	if movement.length() > 0:
+		self.move_and_slide(movement * MOVEMENT_SPEED, Vector3.UP)
--- SandboxScenes/Antti.gd
+++ SandboxScenes/Antti.gd
@@ -16,16 +16,7 @@ func _process(delta):
 	pass	
 
 func _physics_process(delta):
-	var movement = Vector3.ZERO
-	if Input.is_action_pressed("forward"):
-		movement += -player.transform.basis.z
-	if Input.is_action_pressed("right"):
-		player.rotate(Vector3.UP, deg2rad(-5))
-	if Input.is_action_pressed("left"):
-		player.rotate(Vector3.UP, deg2rad(5))
-
-	if movement.length() > 0:
-		player.move_and_slide(movement * 5, Vector3.UP)
+	pass
 	
 func hit_pellet(body):
 	if body.name == "Player":
