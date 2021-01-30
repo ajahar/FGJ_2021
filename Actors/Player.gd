eccc07f99e0f094cd0b648fa16bc82adff933577
Antti Harju <antti.harju@saunalahti.fi>
2021-01-31 01:51:56 +0200

    Add stomping timer to player

--- Actors/Player.gd
+++ Actors/Player.gd
@@ -1,16 +1,24 @@
 extends KinematicBody
 
+onready var stomp_timer: Timer = $StompTimer
+
 const MOVEMENT_SPEED = 8
 const ROTATION_SPEED = 5
 
+var stomping = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	stomp_timer.connect("timeout", self, "stomp_timeout")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("stomp") and !stomping:
+		print("stomping")
+		stomp_timer.start()
+		stomping = true
 
 
 func _physics_process(delta):
@@ -24,3 +32,9 @@ func _physics_process(delta):
 
 	if movement.length() > 0:
 		var collision = self.move_and_collide(movement * MOVEMENT_SPEED * delta)
+		
+
+func stomp_timeout():
+	stomping = false
+	print("stomped very hard")
+	# TODO: Call human?
