4d525421e5d522e1195a99f2393f059a3791de57
Antti Harju <antti.harju@saunalahti.fi>
2021-01-31 02:23:10 +0200

    Add autoloaded audio scene

--- Systems/Audio.gd
+++ Systems/Audio.gd
@@ -1,18 +1,21 @@
-extends Node
-
+extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	GameManager.audio = self
-	play_new_game()
-	start_music()
+	#play_new_game()
+	#start_music()
+	play_tutorial_music()
 	
 func start_music():
 	$Music.play()
 	
 func stop_music():
 	$Music.stop()
-	
+
+func play_tutorial_music():
+	$TutorialMusic.play()
+
 func play_new_game():
 	$Open.play()
 
