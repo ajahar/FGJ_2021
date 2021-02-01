751e0a5d5d1c9eab88e48767386a863ba7f03ada
Timo Koskiahde <timo.koskiahde@iki.fi>
2021-02-02 00:31:51 +0200

    Add some collision geometry to level scene, including at least walls, bookshelf, some bunny toys, tv shelf, sofa etc.

--- GameScenes/Tutorial.gd
+++ GameScenes/Tutorial.gd
@@ -14,7 +14,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_released("ui_accept"):
 		if tutorial_index == 1:
 			tutorial1.visible = false
